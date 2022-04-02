6e807d8ae65ea61f805edd5b4188c7cb7582ed37
Nix-code <nishantbanjade07@gmail.com>
2022-04-02 09:40:26 +0530

    add flask api

---
+++ app.py
@@ -0,0 +1,20 @@
+
+# importing the necessary tools
+from flask import Flask, render_template, request,redirect, url_for, session
+
+# to let flask interact easily while performing file and folder processes irrespective of operating system
+import os
+
+webroot = 'src'
+static_dir = os.path.join(webroot,'static')
+template_dir = os.path.join(webroot,'templates')
+
+app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run(port=5000,debug=True)
